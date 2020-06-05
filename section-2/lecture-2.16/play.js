c871bd96cb4c73e7973c11ae92a247ef6e987fef
Robert Hieger <rhieger@gmail.com>
2020-06-05 15:54:03 -0400

    Correct lecture-2.16 code

--- section-2/lecture-2.16/play.js
+++ section-2/lecture-2.16/play.js
@@ -6,12 +6,4 @@ const person = {
   }
 }
 
-const hobbies = ["Sports", "Cooking"];
-
-for (let hobby of hobbies) {
-  console.log(hobby);
-}
-
-console.log(hobbies.map(hobby => "Hobby: " + hobby));
-
-console.log(hobbies);
+person.greeting();
