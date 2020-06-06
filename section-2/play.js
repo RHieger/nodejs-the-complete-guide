c81f7898a8aeccb58f4fdd26baf48dddf02c08da
Robert Hieger <rhieger@gmail.com>
2020-06-06 00:45:57 -0400

    Demonstrate confusing nested asynchronous code

--- section-2/play.js
+++ section-2/play.js
@@ -1,5 +1,20 @@
 // Demonstrate Asynchronous Code (utilizes callback function):
 
+const fetchData = callback => {
+  setTimeout(() => {
+    console.log("Done!");
+  }, 1500);
+};
+
 setTimeout(() => {
   console.log("Timer is done!");
+  fetchData((text) => {
+    console.log(text);
+  });
 }, 2000);
+
+// Synchronous Code:
+
+console.log("Hello!");
+
+console.log("Hi!");
