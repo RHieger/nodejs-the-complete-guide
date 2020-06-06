8dc638b47dbf724dd607cc6bd875628d97165e21
Robert Hieger <rhieger@gmail.com>
2020-06-06 01:06:45 -0400

    Complete code contrasting sync and async functions

--- section-2/play.js
+++ section-2/play.js
@@ -1,16 +1,23 @@
 // Demonstrate Asynchronous Code (utilizes callback function):
 
-const fetchData = callback => {
-  setTimeout(() => {
-    console.log("Done!");
-  }, 1500);
+const fetchData = () => {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Done!");
+    }, 1500);
+  });
+  return promise;
 };
 
 setTimeout(() => {
   console.log("Timer is done!");
-  fetchData((text) => {
+  fetchData().then((text) => {
     console.log(text);
-  });
+    return fetchData();
+  })
+    .then((text2) => {
+      console.log(text2);
+    });
 }, 2000);
 
 // Synchronous Code:
