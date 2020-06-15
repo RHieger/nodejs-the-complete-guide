8e6440aced89ff4e0062aeed609dac091ae67bb6
Robert Hieger <rhieger@gmail.com>
2020-06-14 21:13:22 -0400

    Remove routing code from app.js and move to routes.js

---
+++ section-3/lecture-3.39/app.js
@@ -0,0 +1,9 @@
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  const method = req.method;
+});
+
+server.listen(3000);
