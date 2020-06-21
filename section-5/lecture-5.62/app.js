fb4d34d3c9aab63b73dfddc422429ba2eb0ca792
Robert Hieger <rhieger@gmail.com>
2020-06-21 12:16:48 -0400

    Complete mini-project: Add response to middleware

--- section-5/lecture-5.62/app.js
+++ section-5/lecture-5.62/app.js
@@ -11,7 +11,7 @@ app.use( (req, res, next) => {
 
 app.use( (req, res, next) => {
   console.log("In another middleware!");
-  // TBD: send a response to browser
+  res.send("<h1>Hello from Express!</h1>");
 });
 
 const server = http.createServer(app);
