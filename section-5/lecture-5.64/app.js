bb94f47b4462ca9d37d59d24cc1d366e4b690a6d
Robert Hieger <rhieger@gmail.com>
2020-06-21 15:35:17 -0400

    Complete mini-project: Configured two paths with Express

--- section-5/lecture-5.64/app.js
+++ section-5/lecture-5.64/app.js
@@ -2,20 +2,17 @@ const express = require("express");
 
 const app = express();
 
+app.use("/", (req, res, next) => {
+  console.log("This always runs!");
+  next();
+});
+
 app.use("/add-product", (req, res, next) => {
   console.log("In the middleware!");
   res.send("<h1>The &ldquo;Add Product&rdquo; Page</h1>");
 });
 
-// NOTE: "/" path simply means path STARTS with "/";
-// the path may continue with any other specified path
-// segment.
 app.use("/", (req, res, next) => {
-  console.log("In the middleware!");
-  next();  // Allows request to proceed to next middleware
-});
-
-app.use( (req, res, next) => {
   console.log("In another middleware!");
   res.send("<h1>Hello from Express!</h1>");
 });
