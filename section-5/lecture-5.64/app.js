7de657372b68b4f8f8c61a27b9b0b31ac7dc4f1c
Robert Hieger <rhieger@gmail.com>
2020-06-21 15:19:43 -0400

    Add in middleware with '/add-product' path

--- section-5/lecture-5.64/app.js
+++ section-5/lecture-5.64/app.js
@@ -2,7 +2,15 @@ const express = require("express");
 
 const app = express();
 
-app.use( (req, res, next) => {
+app.use("/add-product", (req, res, next) => {
+  console.log("In the middleware!");
+  res.send("<h1>The &ldquo;Add Product&rdquo; Page</h1>");
+});
+
+// NOTE: "/" path simply means path STARTS with "/";
+// the path may continue with any other specified path
+// segment.
+app.use("/", (req, res, next) => {
   console.log("In the middleware!");
   next();  // Allows request to proceed to next middleware
 });
