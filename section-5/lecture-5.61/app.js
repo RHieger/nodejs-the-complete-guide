00289fb7e6f093ce2bd183f411d2c313d7f1a9ec
Robert Hieger <rhieger@gmail.com>
2020-06-21 11:53:52 -0400

    Complete mini-project: add 2 middlewares

--- section-5/lecture-5.61/app.js
+++ section-5/lecture-5.61/app.js
@@ -4,6 +4,16 @@ const express = require("express");
 
 const app = express();
 
+app.use( (req, res, next) => {
+  console.log("In the middleware!");
+  next();  // Allows request to proceed to next middleware
+});
+
+app.use( (req, res, next) => {
+  console.log("In another middleware!");
+  // TBD: send a response to browser
+});
+
 const server = http.createServer(app);
 
 server.listen(3000);
