9ec2f3338dcc8fc0903eae7132d3421d80243789
Robert Hieger <rhieger@gmail.com>
2020-07-07 01:31:15 -0400

    Install nodemon as dev dependency and revise start script

--- section-3/assignment-3.1/app.js
+++ section-3/assignment-3.1/app.js
@@ -1,15 +1,11 @@
 const http = require('http');
-const { nextTick } = require('process');
 const port = 3000;
 
 // STEP 1. Spin up a Node.js-driven Server (on port 3000)
 
 const httpServer = http.createServer( (request, response) => {
-  response.write('<h1>Hi, I\'m a Node.js HTTP Server!</h1>');
   
 });
-
-
 
 httpServer.listen(port, () => {
   console.log(`\nNode.js HTTP Server listening on Port ${port}...\n`);
