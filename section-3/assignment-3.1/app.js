566030920fb6126aec79e949742c6086c7743306
Robert Hieger <rhieger@gmail.com>
2020-07-08 21:19:18 -0400

    Complete Step 2 of assignment-3.1

--- section-3/assignment-3.1/app.js
+++ section-3/assignment-3.1/app.js
@@ -3,8 +3,36 @@ const port = 3000;
 
 // STEP 1. Spin up a Node.js-driven Server (on port 3000)
 
+/*
 const httpServer = http.createServer( (request, response) => {
   
+});
+
+httpServer.listen(port, () => {
+  console.log(`\nNode.js HTTP Server listening on Port ${port}...\n`);
+});
+*/
+
+// STEP 2. Add two routes: '/' and '/users'
+// '/' should respond with greeting of choice.
+// '/users' should respond with fictitious user
+// list in unordered list.
+
+const httpServer = http.createServer( (request, response) => {
+  const url = request.url;
+  const method = request.method;
+  
+  if (url === '/') {
+    response.write(`<html>
+      <head>
+        <title>Simple Node.js Server</title>
+      </head>
+      <body>
+        <h1>Hi, I'm the Node.js HTTP Server!</h1>
+      </body>
+    </html>`);
+    return response.end();
+  }
 });
 
 httpServer.listen(port, () => {
