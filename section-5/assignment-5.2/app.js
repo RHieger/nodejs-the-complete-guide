699d55460318de333600acb62a25c2f2910066b1
Robert Hieger <rhieger@gmail.com>
2020-07-10 16:18:36 -0400

    Complete Step 1: assignnment-5.2

--- section-5/assignment-5.2/app.js
+++ section-5/assignment-5.2/app.js
@@ -1,3 +1,35 @@
+// STEP 1. Create npm project and install Express;
+// also install nodemon
+
 const express = require('express');
 
 const app = express();
+
+const port = 3000;
+
+app.use('/', (request, response, next) => {
+  console.log('Hello from my first Middleware!');
+  next();
+});
+
+app.use('/users', (request, response, next) => {
+  console.log('Hello from my second middleware!');
+  response.send(`<html>
+    <head>
+      <title>User List</title>
+    </head>
+    <body>
+      <h1>User List</h1>
+      <ul>
+        <li>Charles Babbage</li>
+        <li>Ada Lovelace</li>
+        <li>Alan Turing</li>
+        <li>Sir Tim Berners Lee</li>
+      </ul>
+    </body>`);
+    next();
+});
+
+app.listen(port, () => {
+  console.log(`HTPP Server listening at Port ${port}...`);
+});
