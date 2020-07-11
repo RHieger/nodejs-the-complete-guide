6f011b8311438557f7d79556a9fb8bab8664422d
Robert Hieger <rhieger@gmail.com>
2020-07-11 10:14:06 -0400

    Complete lecture-5.64: Add middleware to parse HTTP request body

--- section-5/lecture-5.64/app.js
+++ section-5/lecture-5.64/app.js
@@ -3,12 +3,21 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use();
+// body-parser was deprecated in 2014 because Express 4.x 
+// includes its own internal parser. However, body-parser may
+// still be used if the 'extended' parameter is set, and will
+// prevent warning or error messages. See StackOverflow:
+// https://stackoverflow.com/questions/24330014/bodyparser-is-deprecated-express-4
+// Also see body-parser documentation:
+// https://www.npmjs.com/package/body-parser#extended
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 
 app.use("/add-product", (req, res, next) => {
   res.send(`<form action="/product" method="POST">
   <input type="text" name="title">
-  <button style="cursor: poiter;" type="submit">
+  <button style="cursor: pointer;" type="submit">
     Add Product
   </button>
 </form>`);
