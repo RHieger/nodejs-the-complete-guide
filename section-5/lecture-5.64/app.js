d1542dc36eca946a1cbfd37495f35d54cae1a6e9
Robert Hieger <rhieger@gmail.com>
2020-07-11 09:42:39 -0400

    Add body-parser package as production dependency: lecture-5.64

--- section-5/lecture-5.64/app.js
+++ section-5/lecture-5.64/app.js
@@ -1,14 +1,11 @@
 const express = require("express");
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use("/", (req, res, next) => {
-  console.log("This always runs!");
-  next();
-});
+app.use();
 
 app.use("/add-product", (req, res, next) => {
-  console.log("In the middleware!");
   res.send(`<form action="/product" method="POST">
   <input type="text" name="title">
   <button style="cursor: poiter;" type="submit">
@@ -17,8 +14,12 @@ app.use("/add-product", (req, res, next) => {
 </form>`);
 });
 
+app.use('/product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
+});
+
 app.use("/", (req, res, next) => {
-  console.log("In another middleware!");
   res.send("<h1>Hello from Express!</h1>");
 });
 
