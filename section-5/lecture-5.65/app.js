daea5fd60c06369d935c82526d7999d44ed13a66
Robert Hieger <rhieger@gmail.com>
2020-07-11 10:46:57 -0400

    Complete lecture-5.65: Refactor '/product' middleware to accept POST only

--- section-5/lecture-5.65/app.js
+++ section-5/lecture-5.65/app.js
@@ -23,7 +23,7 @@ app.use("/add-product", (req, res, next) => {
 </form>`);
 });
 
-app.use('/product', (req, res, next) => {
+app.post('/product', (req, res, next) => {
   console.log(req.body);
   res.redirect('/');
 });
