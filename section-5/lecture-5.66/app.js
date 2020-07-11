123d6c783b6198674cdcf53709b9c00ee567e1c8
Robert Hieger <rhieger@gmail.com>
2020-07-11 13:33:34 -0400

    Move admin router functions into admin.js: lecture-5.66

--- section-5/lecture-5.66/app.js
+++ section-5/lecture-5.66/app.js
@@ -14,20 +14,6 @@ app.use(bodyParser.urlencoded({
   extended: false
 }));
 
-app.use("/add-product", (req, res, next) => {
-  res.send(`<form action="/product" method="POST">
-  <input type="text" name="title">
-  <button style="cursor: pointer;" type="submit">
-    Add Product
-  </button>
-</form>`);
-});
-
-app.post('/product', (req, res, next) => {
-  console.log(req.body);
-  res.redirect('/');
-});
-
 app.use("/", (req, res, next) => {
   res.send("<h1>Hello from Express!</h1>");
 });
