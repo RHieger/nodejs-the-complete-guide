67c27aabd88d09df96931af74667533bf772aa28
Robert Hieger <rhieger@gmail.com>
2020-07-11 01:15:39 -0400

    Completed Step 3 for assignment-5.2

--- section-5/assignment-5.2/app.js
+++ section-5/assignment-5.2/app.js
@@ -2,6 +2,7 @@
 // also install nodemon
 
 const express = require('express');
+const { response } = require('express');
 
 const app = express();
 
@@ -10,6 +11,8 @@ const port = 3000;
 // STEP 2. Create an Express app that funnels requests through
 // 2 middleware functions that log something to console and
 // return one respoonse.
+
+/* Commented out in favor of refactored middleware below.
 
 app.use( (request, response, next) =>  {
   console.log('Hello from my first Middleware!');
@@ -26,6 +29,43 @@ app.use( (request, response, next) => {
       <h1 style="color: blue;">Welcome to My Page!</h1>
     </body>
   </html>`);
+});
+
+*/
+
+// STEP 3. Handle requests to '/' and '/users' such that each
+// request only has one middleware that does something with
+// it (e.g. send dummy reponse).
+
+app.use('/users', (request, response, next) => {
+  console.log('Sending User List Page...');
+  response.send(`<html>
+  <head>
+    <title>User List</title>
+  </head>
+  <body>
+    <h1 style="color: blue;">User List</h1>
+    <ul style="list-style-type: square;
+    font-size: 21px; color: blue;">
+      <li>Charles Babbage</li>
+      <li>Ada Lovelace</li>
+      <li>Alan Turing</li>
+      <li>Sir Tim Berners-Lee</li>
+    </ul>
+  </body>
+</html>`);
+});
+
+app.use('/', (request, response, next) => {
+  console.log('Sending Landing Page...');
+  response.send(`<html>
+  <head>
+    <title>Welcome</title>
+  </head>
+  <body>
+    <h1 style="color: blue;">Welcome to my Landing Page!</h1>
+  </body>
+</html>`);
 });
 
 app.listen(port, () => {
