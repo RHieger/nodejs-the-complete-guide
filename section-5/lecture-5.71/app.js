76f60eaacc8ae91df301e8c163d9832b7507904c
Robert Hieger <rhieger@gmail.com>
2020-07-12 16:26:11 -0400

    Complete lecture-5.71: issue report redirect does not work

--- section-5/lecture-5.71/app.js
+++ section-5/lecture-5.71/app.js
@@ -1,31 +1,20 @@
-const express = require("express");
+const path = require('path');
+
+const express = require('express');
 const bodyParser = require('body-parser');
+
+const app = express();
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-const port = 3000;
-const app = express();
-
-// body-parser was deprecated in 2014 because Express 4.x 
-// includes its own internal parser. However, body-parser may
-// still be used if the 'extended' parameter is set, and will
-// prevent warning or error messages. See StackOverflow:
-// https://stackoverflow.com/questions/24330014/bodyparser-is-deprecated-express-4
-// Also see body-parser documentation:
-// https://www.npmjs.com/package/body-parser#extended
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-// 404 Error Handler
-app.use( (request, response, next) => {
-  response.status(404).send(``);
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
-app.listen(port, () => {
-  console.log(`HTTP Server Listening on Port ${port}...`)
-});
+app.listen(3000);
--- section-5/lecture-5.71/routes/admin.js
+++ section-5/lecture-5.71/routes/admin.js
@@ -5,14 +5,13 @@ const express = require('express');
 const router = express.Router();
 
 // /admin/add-product => GET
-router.get("/add-product", (req, res, next) => {
+router.get('/add-product', (req, res, next) => {
   res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
 });
 
 // /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
   console.log(req.body);
-  // TODO: Fix error--unexpectedly returns 404 status
   res.redirect('/');
 });
 
