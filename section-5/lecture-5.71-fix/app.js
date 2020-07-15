5d0777cf405fc6520183dc845a54cd2ec2a6418f
Robert Hieger <rhieger@gmail.com>
2020-07-15 00:30:34 -0400

    Add path cor package and body-parser for HTTP requests

--- section-5/lecture-5.71-fix/app.js
+++ section-5/lecture-5.71-fix/app.js
@@ -1,7 +1,12 @@
+const path = require('path');
 const http = require('http');
+
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+// TBD: Add in routing
 
 const port = 3000;
 
