9822b7ae26083591a97405b541f0c1f9d56536e5
Robert Hieger <rhieger@gmail.com>
2020-07-15 21:00:07 -0400

    Add route for '/shop'

--- section-5/lecture-5.71-fix/app.js
+++ section-5/lecture-5.71-fix/app.js
@@ -8,10 +8,12 @@ const port = 3000;
 const app = express();
 
 const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/', adminRoutes);
+app.use(shopRoutes);
 
 app.listen(port, () => {
   console.log(`HTTP Server listening on Port ${port}...`);
--- section-5/lecture-5.71-fix/routes/admin.js
+++ section-5/lecture-5.71-fix/routes/admin.js
@@ -9,4 +9,10 @@ router.get('/add-product', (req, res, next) => {
   res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
 });
 
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
+});
+
 module.exports = router;
