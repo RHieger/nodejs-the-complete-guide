bdd51580d56eef64ed61d56abd74281bb459d9ce
Robert Hieger <rhieger@gmail.com>
2020-07-16 00:25:14 -0400

    Complete lecture-5.71-fix: Add route for 404 error handling

--- section-5/lecture-5.71-fix/app.js
+++ section-5/lecture-5.71-fix/app.js
@@ -15,6 +15,10 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use('/', adminRoutes);
 app.use(shopRoutes);
 
+app.use( (req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
 app.listen(port, () => {
   console.log(`HTTP Server listening on Port ${port}...`);
 });
