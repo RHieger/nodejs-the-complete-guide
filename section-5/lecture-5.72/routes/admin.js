96cae3d5796c4d3feb112c391f16840db629b6e8
Robert Hieger <rhieger@gmail.com>
2020-07-17 15:42:04 -0400

    Add path.js helper function

--- section-5/lecture-5.72/routes/admin.js
+++ section-5/lecture-5.72/routes/admin.js
@@ -5,12 +5,12 @@ const express = require('express');
 const router = express.Router();
 
 // /admin/add-product => GET
-router.get('/add-product', (req, res, next) => {
+router.get('/admin/add-product', (req, res, next) => {
   res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
 });
 
 // /admin/add-product => POST
-router.post('/add-product', (req, res, next) => {
+router.post('/admin/add-product', (req, res, next) => {
   console.log(req.body);
   res.redirect('/');
 });
