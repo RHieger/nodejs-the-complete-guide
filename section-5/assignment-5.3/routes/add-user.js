2d968a109e5ece4cdd7454faf4d07693c2c7b78c
Robert Hieger <rhieger@gmail.com>
2020-07-18 15:27:32 -0400

    Added POST route to Add User Landing page

--- section-5/assignment-5.3/routes/add-user.js
+++ section-5/assignment-5.3/routes/add-user.js
@@ -9,4 +9,10 @@ addUserRouter.get('/', (request, response, next) => {
   response.sendFile(path.join(__dirname, '..', 'views', 'add-user.html'));
 });
 
+addUserRouter.post('/add-user', (request, response, next) => {
+  console.log('Posting new user data...');
+  console.log(request.body);
+  response.redirect('/');
+});
+
 module.exports = addUserRouter;
--- section-5/assignment-5.3/routes/users.js
+++ section-5/assignment-5.3/routes/users.js
@@ -5,7 +5,7 @@ const express = require('express');
 const usersRouter = express.Router();
 
 usersRouter.get('/users', (request, response, next) => {
-  console.log('Serving Landing page...');
+  console.log('Serving User List page...');
   response.sendFile(path.join(__dirname, '..', 'views', 'users.html'));
 });
 
