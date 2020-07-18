ce8e10edf9289d8e29ce1c1bd71db9e5a010474a
Robert Hieger <rhieger@gmail.com>
2020-07-17 21:19:17 -0400

    Refactor app.js to serve CSS files statically from "public/css"

--- section-5/lecture-5.74/app.js
+++ section-5/lecture-5.74/app.js
@@ -11,6 +11,7 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', adminRoutes);
 app.use(shopRoutes);
