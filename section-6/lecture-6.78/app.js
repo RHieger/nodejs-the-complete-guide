45168ebc0a9ea3e59c2465df8930755c1a9dd22f
Robert Hieger <rhieger@gmail.com>
2020-07-19 19:29:04 -0400

    Complete Lecture 6.78: Refactor routes so POST relies on array data

--- section-6/lecture-6.78/app.js
+++ section-6/lecture-6.78/app.js
@@ -7,13 +7,13 @@ const port = 3000;
 
 const app = express();
 
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', adminRoutes);
+app.use('/', adminData.routes);
 app.use(shopRoutes);
 
 app.use( (req, res, next) => {
