065607792d6b116ba6b5476a6797e6ed54f1bec2
Robert Hieger <rhieger@gmail.com>
2020-07-25 01:37:20 -0400

    Add sample static content for Shop page

--- section-6/lecture-6.81/routes/shop.js
+++ section-6/lecture-6.81/routes/shop.js
@@ -8,7 +8,9 @@ const adminData = require('./admin');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.render('shop');
+  const products = adminData.products;
+  console.log(products);
+  res.render('shop', {prods: products, docTitle: 'Shop'});
 });
 
 module.exports = router;
