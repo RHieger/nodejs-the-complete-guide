d9249f3b2e45465fd716b8f1781a71653529066e
Robert Hieger <rhieger@gmail.com>
2020-07-25 14:56:49 -0400

    Migrate code from lecture-6.83 to lecture-6.84

---
+++ section-6/lecture-6.84/app.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const port = 3000;
+
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', adminData.routes);
+app.use(shopRoutes);
+
+app.use( (req, res, next) => {
+  res.status(404).render('404', {docTitle: 'Page Not Found'});
+});
+
+app.listen(port, () => {
+  console.log(`HTTP Server listening on Port ${port}...`);
+});
